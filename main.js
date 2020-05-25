30d7716afcab1d71d67aefd4890949bacc00e079
Tomasz Fryze <t.fryze@gmail.com>
2020-05-25 22:14:28 +0200

    BEM implementing

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-const name = 'Tomek';
+const name = "Tomek";
 const age = 28;
 
 console.log("Elo mordo!");
 console.log(`Nazywam się ${name} i mam ${age} lat.`);
+
+const navigationLink = document.querySelector('.navigation__link--js');
+
+console.log(navigationLink.innerHTML);
+
+navigationLink.innerHTML = "Elo elo";
+
+console.log(navigationLink.innerHTML);
